e15a5bfdf07737bc0fa45651c37ac69ab5bed7dc
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2024-03-11 23:32:59 -0500

    Initialized the Intro page with some projects presented

---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+
+// Store the project information here
+export default [
+    {
+        title: "Terminal Website",
+        imgURL: "",
+        stack: ["HTML", "CSS", "JavaScript"],
+        link: "https://github.com/Geo-Li",
+    },
+    {
+        title: "Developer Portfolio",
+        imgURL: "",
+        stack: ["HTML", "CSS", "JavaScript"],
+        link: "https://github.com/Geo-Li",
+    },
+    {
+        title: "Motor Control Project",
+        imgURL: "",
+        stack: ["C", "Python", "Breadboard", "Mechatronics"],
+        link: "https://github.com/Geo-Li",
+    }
+]

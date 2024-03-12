de7062692793474d18ad4965d372563ff35ec435
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2024-03-12 01:10:17 -0500

    Wrote timeline section and inserted some information for timelines

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,22 +1,21 @@
-
 // Store the project information here
 export default [
-    {
-        title: "Terminal Website",
-        imgURL: "public/assets/Bread Project.png",
-        stack: ["HTML", "CSS", "JavaScript"],
-        link: "https://github.com/Geo-Li",
-    },
-    {
-        title: "Developer Portfolio",
-        imgURL: "public/assets/Elastic Search.png",
-        stack: ["HTML", "CSS", "JavaScript"],
-        link: "https://github.com/Geo-Li",
-    },
-    {
-        title: "Motor Control Project",
-        imgURL: "public/assets/SimpleDB.png",
-        stack: ["C", "Python", "Breadboard", "Mechatronics"],
-        link: "https://github.com/Geo-Li",
-    }
-]
+  {
+    title: "Terminal Website",
+    imgURL: "public/assets/Bread Project.png",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://github.com/Geo-Li",
+  },
+  {
+    title: "Developer Portfolio",
+    imgURL: "public/assets/Elastic Search.png",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://github.com/Geo-Li",
+  },
+  {
+    title: "Motor Control Project",
+    imgURL: "public/assets/SimpleDB.png",
+    stack: ["C", "Python", "Breadboard", "Mechatronics"],
+    link: "https://github.com/Geo-Li",
+  },
+];
---
+++ src/data/timeline.js
@@ -0,0 +1,21 @@
+// Store the career timeline information here
+export default [
+  {
+    year: "Today",
+    title: "Master's Student",
+    duration: "6 months",
+    details: "Northwestern University",
+  },
+  {
+    year: "Today",
+    title: "Master's Student",
+    duration: "6 months",
+    details: "Northwestern University",
+  },
+  {
+    year: "Today",
+    title: "Master's Student",
+    duration: "6 months",
+    details: "Northwestern University",
+  },
+];

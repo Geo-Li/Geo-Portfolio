e25336f670ba5646e2c48bfeb03127f45d9f81ac
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2024-03-15 02:18:09 -0500

    Update img path so when deployed, they will show correctly

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,19 +2,19 @@
 export default [
   {
     title: "Terminal Website",
-    imgURL: "public/assets/Bread Project.png",
+    imgURL: "assets/Bread Project.png",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://github.com/Geo-Li",
   },
   {
     title: "Developer Portfolio",
-    imgURL: "public/assets/Elastic Search.png",
+    imgURL: "assets/Elastic Search.png",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://github.com/Geo-Li",
   },
   {
     title: "Motor Control Project",
-    imgURL: "public/assets/SimpleDB.png",
+    imgURL: "assets/SimpleDB.png",
     stack: ["C", "Python", "Breadboard", "Mechatronics"],
     link: "https://github.com/Geo-Li",
   },

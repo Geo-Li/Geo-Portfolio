990ca5141b31431f00a4deed1d7d3ffb29e60a2e
Geo Li <zhuoyul4@uci.edu>
2024-03-25 12:19:58 -0500

    Update the timeline information

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -5,19 +5,23 @@ export default [
     title: "Master of Science in Computer Science",
     duration: "6 months",
     institution: "Northwestern University",
-    details: "Northwestern University",
+    details:
+      "Majored in computer science. Expected graduation date: December 2024",
   },
   {
     title: "Research Assistant",
     duration: "6 months",
     institution: "Whiskit Physics Lab",
-    details: "Northwestern University",
+    details:
+      "For the first quarter, I am responsible for using machine learning model, \
+      for instance, neural network, to classify whether the rat's whiskers have contacted the object",
   },
   {
     year: "2019",
     title: "Bachelor of Computer Science",
     duration: "4 years",
     institution: "University of California, Irvine",
-    details: "Majored in Computer Science, specified in Artificial Intelligence",
+    details:
+      "Majored in Computer Science, specified in Artificial Intelligence",
   },
 ];

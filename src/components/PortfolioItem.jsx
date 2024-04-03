50efd9b263dd95c701e4c5201bbf817cecb01ddb
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2024-04-02 21:48:42 -0500

    Added expand animation when hover over

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PortfolioItem = ({ title, imgURL, stack, link }) => {
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  const zoomStyle = isZoomed
+    ? "fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2\
+       transition ease-in-out delay-200 scale-150 z-50 duration-300\
+       overflow-hidden bg-black"
+    : "";
+
+  function toggleZoom() {
+    setIsZoomed(!isZoomed);
+  }
+
   return (
     <a
-      className="border-2 border-stone-900
-               dark:border-white
+      className="transition ease-in-out delay-150
+                 hover:scale-110 duration-300
+                 hover:bg-blue-100
+                 hover:dark:bg-emerald-300
+                 border-2 border-stone-900
+                 dark:border-white
                  rounded-md overflow-hidden"
       href={link}
       target="_blank"
       rel="noopener noreferrer"
+      onClick={toggleZoom}
     >
       <img
         src={imgURL}
@@ -17,8 +34,10 @@ const PortfolioItem = ({ title, imgURL, stack, link }) => {
                    object-contain cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3 className="text-lg md:text-xl dark:text-white
-                       mb-2 md:mb-3 font-semibold">
+        <h3
+          className="text-lg md:text-xl dark:text-white
+                     mb-2 md:mb-3 font-semibold"
+        >
           {title}
         </h3>
         <p
